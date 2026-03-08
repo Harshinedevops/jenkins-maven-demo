0e4b708507e62b3db9ab5449088c759a94264c93
Harshine <harshinivedhanayagam@gmail.com>
2026-03-08 23:56:41 +0530

    Create App.java

---
+++ src/main/java/com/emc/App.java
@@ -0,0 +1,9 @@
+package com.emc;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello EMC Students!");
+        System.out.println("CI/CD Pipeline is Working!");
+        System.out.println("Jenkins + GitHub + Maven = Success!");
+    }
+}
